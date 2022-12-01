8d46c343cac09a4a1c18ec9b1100e6a4c55b0d09
Esther Yoon <estheryoon@wireless-10-145-13-121.public.utexas.edu>
2022-12-01 13:38:22 -0600

    add steering wheel to scene + scripts

---
+++ Scripts/SteeringWheelControl.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+using Vector3 = UnityEngine.Vector3;
+using Quaternion = UnityEngine.Quaternion;
+
+public class SteeringWheelControl : MonoBehaviour
+{
+    // Right Hand
+    public GameObject rightHand;
+    private Transform rightHandOriginalParent;
+    private bool rightHandOnWheel = false;
+    // Left Hand
+    public GameObject leftHand;
+    private Transform leftHandOriginalParent;
+    private bool leftHandOnWheel = false;
+
+    public Transform[] snapPositions;
+
+    // Wheel + objects to control with wheel
+    public GameObject Vehicle; // represents the car
+    private Rigidbody VehicleRigidBody;
+
+    public float currentWheelRotation = 0.0f;
+
+    private float turnDampening = 9999;
+
+    // TODO make this directly in the middle by making it a
+    // child of steering wheel, then not a child anymore.
+    public Transform steeringWheelDirectionalObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VehicleRigidBody = Vehicle.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReleaseHandsFromWheel();
+        ConvertHandRotationToSteeringWheelRotation();
+        TurnVehicle();
+        currentWheelRotation = -transform.rotation.eulerAngles.z;
+    }
+
+    private void ReleaseHandsFromWheel()
+    {
+        if (rightHandOnWheel && OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+        {
+            rightHand.transform.parent = rightHandOriginalParent;
+            rightHand.transform.position = rightHandOriginalParent.position;
+            rightHand.transform.rotation = rightHandOriginalParent.rotation;
+            rightHandOnWheel = false;
+        }
+
+        if (leftHandOnWheel && OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
+        {
+            leftHand.transform.parent = leftHandOriginalParent;
+            leftHand.transform.position = leftHandOriginalParent.position;
+            leftHand.transform.rotation = leftHandOriginalParent.rotation;
+            leftHandOnWheel = false;
+        }
+
+        if (!leftHandOnWheel && !rightHandOnWheel)
+        {
+            transform.parent = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // TODO add box collider so this works
+        if (other.CompareTag("PlayerHand"))
+        {
+            // Place right hand
+            if (rightHandOnWheel == false && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+            {
+                PlaceHandOnWheel(ref rightHand, ref rightHandOriginalParent, ref rightHandOnWheel);            
+            }
+            // Place left hand
+            if (leftHandOnWheel == false && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
+            {
+                PlaceHandOnWheel(ref leftHand, ref leftHandOriginalParent, ref leftHandOnWheel);
+            }
+        }
+    }
+
+    private void PlaceHandOnWheel(ref GameObject hand, ref Transform originalParent, ref bool handOnWheel)
+    {
+        var shortestDistance = Vector3.Distance(snapPositions[0].position, hand.transform.position);
+        var bestSnap = snapPositions[0];
+        foreach (var snapPosition in snapPositions)
+        {
+            if (snapPosition.childCount == 0)
+            {
+                var distance = Vector3.Distance(snapPosition.position, hand.transform.position);
+                if (distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    bestSnap = snapPosition;
+                }
+            }
+        }
+           
+        originalParent = hand.transform.parent;
+
+        hand.transform.parent = bestSnap.transform;
+        hand.transform.position = bestSnap.transform.position;
+        handOnWheel = true;
+        
+    }
+
+    private void TurnVehicle()
+    {
+        // Turn wheels compared to the steering wheel
+        var turn = -transform.rotation.eulerAngles.z;
+        if (turn < -350)
+        {
+            turn = turn + 360;
+        }
+
+        VehicleRigidBody.MoveRotation(Quaternion.RotateTowards(Vehicle.transform.rotation, Quaternion.Euler(0, turn, 0), Time.deltaTime * turnDampening));
+    }
+
+    private void ConvertHandRotationToSteeringWheelRotation()
+    {
+        if (rightHandOnWheel && !leftHandOnWheel)
+        {
+            Quaternion newRot = Quaternion.Euler(0, 0, rightHandOriginalParent.transform.rotation.eulerAngles.z);
+            steeringWheelDirectionalObject.rotation = newRot;
+            transform.parent = steeringWheelDirectionalObject;
+        }
+        else if (!rightHandOnWheel && leftHandOnWheel)
+        {
+            Quaternion newRot = Quaternion.Euler(0, 0, leftHandOriginalParent.transform.rotation.eulerAngles.z);
+            steeringWheelDirectionalObject.rotation = newRot;
+            transform.parent = steeringWheelDirectionalObject;
+        }
+        else if (rightHandOnWheel && leftHandOnWheel)
+        {
+            Quaternion newLeftRot = Quaternion.Euler(0, 0, leftHandOriginalParent.transform.rotation.eulerAngles.z);
+            Quaternion newRightRot = Quaternion.Euler(0, 0, rightHandOriginalParent.transform.rotation.eulerAngles.z);
+            Quaternion finalRot = Quaternion.Slerp(newLeftRot, newRightRot, 1.0f / 2.0f);
+            steeringWheelDirectionalObject.rotation = finalRot;
+            transform.parent = steeringWheelDirectionalObject;
+        }
+    }
+}
